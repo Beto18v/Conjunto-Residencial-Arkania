73316790199d4a203b7c59ef27eaaf1aa153464c
ThomasEspinosa86 <thomaspp0105@gmail.com>
2025-09-06 10:27:34 -0500

    Primer commit

---
+++ src/main/java/com/exe/ConjuntoResidencialArkania/ConjuntoResidencialArkaniaApplication.java
@@ -0,0 +1,14 @@
+package com.exe.ConjuntoResidencialArkania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjuntoResidencialArkaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjuntoResidencialArkaniaApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/exe/ConjuntoResidencialArkania/ConjuntoResidencialArkaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.ConjuntoResidencialArkania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjuntoResidencialArkaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
