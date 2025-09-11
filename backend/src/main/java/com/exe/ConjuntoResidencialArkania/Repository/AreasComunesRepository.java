aeadf4480a6ef277835d3b900cd9c8484b3b3363
beto18v <nicolasalberto18@hotmail.com>
2025-09-11 10:11:27 -0500

    Se corrije error de compilacion en AreasComunesRepository.java

--- backend/src/main/java/com/exe/ConjuntoResidencialArkania/Repository/AreasComunesRepository.java
+++ backend/src/main/java/com/exe/ConjuntoResidencialArkania/Repository/AreasComunesRepository.java
@@ -11,8 +11,8 @@ import com.exe.ConjuntoResidencialArkania.Entity.AreasComunesEntity;
 public interface AreasComunesRepository extends JpaRepository<AreasComunesEntity, Long> {
     //Obtener areas comunes activas con capacidad mayor a un valor dado, ordenadas por nombre
     @Query("SELECT a FROM AreasComunesEntity a " +
-            "WHERE a.estado = 'activa' AND a.capacidadMaxima > :capacidad" +
-            "ORDER BY a.nombre ASC")
+        "WHERE a.estado = 'activa' AND a.capacidadMaxima > :capacidad " +
+        "ORDER BY a.nombre ASC")
     List<AreasComunesEntity> findAllActiveAreasOrderedByName(@Param("capacidad") int capacidad);
 
 
