027647b5042c3228880be83798389a9adfc40a02
beto18v <nicolasalberto18@hotmail.com>
2025-09-11 00:37:53 -0500

    Se configura el pom.xml para aceptar db mysql (se agrega la dependencia de mysql-connector-j) se configura el entorno application.properties para conectar con la db mysql

--- src/main/java/com/exe/ConjuntoResidencialArkania/Entity/AreasComunesEntity.java
+++ src/main/java/com/exe/ConjuntoResidencialArkania/Entity/AreasComunesEntity.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
